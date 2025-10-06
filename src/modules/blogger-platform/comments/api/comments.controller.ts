96164d4129380529ce97f62db9671d23fbcd776c
Sabir <magsabir95@gmail.com>
2025-10-06 22:50:52 +0300

    feat(comments): add comments methods

--- src/modules/blogger-platform/comments/api/comments.controller.ts
+++ src/modules/blogger-platform/comments/api/comments.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param } from '@nestjs/common';
+import { UpdateCommentLikeStatusCommand } from '../application/usecases/update-comment-like-status.usecase';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { LikeStatus } from '../../posts/application/view-dto/post-view-model';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  constructor(
+    private queryBus: QueryBus,
+    private commandBus: CommandBus,
+  ) {}
+
+  async updateCommentLike(@Param('id') id: string, @Body() status: LikeStatus) {
+    return this.commandBus.execute(
+      new UpdateCommentLikeStatusCommand(id, status),
+    );
+  }
+}
